c1b080f94daaa18dddadcd8f0214a8c268099610
venkatesh171 <munagavenkatesh9@gmail.com>
2023-10-13 14:23:46 -0400

    .

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,8 +1,14 @@
 import BookShow from "./BookShow";
+import { useContext } from "react";
+import BooksContext from "../context/books";
 
 export default function BookList ({books, onDelete, onUpdate}) {
+    const value = useContext(BooksContext);
     const renderBooks = books.map((book) => {
         return <BookShow key={book.id} book={book} onDelete={onDelete} onUpdate={onUpdate}/>;
     });
-    return <div className="book-list">{renderBooks}</div>
+    return <div className="book-list">
+        {value}
+        {renderBooks}
+    </div>;
 }
